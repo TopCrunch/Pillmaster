4c2ec45f12dcc917af02ab48befdd57a521c35f3
pillmasterteam3 <133179190+pillmasterteam3@users.noreply.github.com>
2023-05-11 17:39:31 +0100

    Add files via upload

---
+++ pillmaster.py
@@ -0,0 +1,39 @@
+import serial
+import tkinter as tk
+import tkinter.messagebox
+from time import sleep
+ser = serial.Serial("/dev/ttyACM0", 9600)
+
+window = tk.Tk()
+tk.messagebox.showinfo("Initializing...", ser.read(90))
+def operatea():
+        ser.write(bytes("a", 'utf-8'))
+        tk.messagebox.showinfo("Container A", ser.readline())
+
+def operateb():
+        ser.write(bytes("b", 'utf-8'))
+        tk.messagebox.showinfo("Container B", ser.readline())
+  
+def operatec():
+        ser.write(bytes("c", 'utf-8'))
+        tk.messagebox.showinfo("Container C", ser.readline())
+        
+def operatew():
+        ser.write(bytes("w", 'utf-8'))
+        tk.messagebox.showinfo("Reading Weight", ser.readline())
+        
+def operatex():
+        ser.write(bytes("x", 'utf-8'))
+        tk.messagebox.showinfo("Finished Reading Weight", ser.readline())
+        
+buttona = tk.Button(master = window, text = 'a', command = operatea)
+buttonb = tk.Button(master = window, text = 'b', command = operateb)
+buttonc = tk.Button(master = window, text = 'c', command = operatec)
+buttonw = tk.Button(master = window, text = 'w', command = operatew)
+buttonx = tk.Button(master = window, text = 'x', command = operatex)
+buttona.pack()
+buttonb.pack()
+buttonc.pack()
+buttonw.pack()
+buttonx.pack()
+window.mainloop()
